e1e2101a88628eb1cce0213d1d1fca2aa0c8e63d
Thibaut SORIANO <st405262@etu.unice.fr>
2016-11-25 16:28:13 +0100

    interface d'appel GUI <=> code

---
+++ Viewer/Version-A/materials-cpp/callinterface.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <QObject>
+
+class CallInterface : public QObject {
+	Q_OBJECT
+
+public:
+	CallInterface(QObject * parent = Q_NULLPTR);
+	~CallInterface();
+
+	void btnLoadModelClicked();
+	
+
+private:
+	
+};
