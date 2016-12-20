59a185fa67d22d10eacbf5bb755b5c4cc6602b62
Antoine ROLLIN <antoine.rollin26@free.fr>
2016-12-20 22:29:49 +0100

    ADD Unit test for capture

--- Capturer/TestCapture/unittest1.cpp
+++ Capturer/TestCapture/unittest1.cpp
@@ -1,9 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../CaptureExplorer/KinectFusionExternals.h"
 
-#include <iostream>
-#include <string>
+#include "../CaptureExplorer/KinectFusionExternals.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,13 +9,45 @@ namespace TestCapture
 {		
 	TEST_CLASS(UnitTest1)
 	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Assert::IsTrue(KinectFusionExternals::fileExists(L"./unittest1.cpp"));
-			Assert::IsFalse(KinectFusionExternals::fileExists(L"./unittest1000.cpp"));
+
+	private:
+		CREATE_CONF_EXT * sConf;
+		void init_create() {
+			sConf = (CREATE_CONF_EXT *)malloc(sizeof(CREATE_CONF_EXT));
+			std::wstring wsVPM = L"wsVPM";
+			sConf->wsVPM = &wsVPM;
+			std::wstring wsX = L"wsX";
+			sConf->wsX = &wsX;
+			std::wstring wsY = L"wsY";
+			sConf->wsY = &wsY;
+			std::wstring wsZ = L"wsZ";
+			sConf->wsZ = &wsZ;
 		}
 
-	};
+	public:
+				
+		TEST_METHOD(FileExistsFalse)
+		{
+			Assert::IsFalse(KinectFusionExternals::fileExists(L"./woua"));
+		}
+
+		TEST_METHOD(FileExistsTrue)
+		{
+			std::wstring wsPath(L"./test.txt");
+
+			// Create file
+			std::wofstream confFile(wsPath, std::ios::out | std::ios::trunc);
+
+			Assert::IsTrue(KinectFusionExternals::fileExists(wsPath));
+
+			DeleteFile(wsPath.c_str());
+		}
+
+		TEST_METHOD(CreateImportFileTrue)
+		{
+			init_create();
+
+		}
+	};			
+
 }
