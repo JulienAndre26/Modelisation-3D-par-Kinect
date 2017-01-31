79a40117b6767e239493d9a7eaac0315a78ca4f2
Antoine ROLLIN <antoine.rollin26@free.fr>
2017-01-31 14:47:04 +0100

    FIX Test project for Viewer

--- Viewer/TestViewer/ProcessorTest.cpp
+++ Viewer/TestViewer/ProcessorTest.cpp
@@ -1,22 +1,22 @@
 //#include "stdafx.h"
 #include "CppUnitTest.h"
 
-//#include "../ViewerExplorer/headers/Processor/Processor.h"
-//
-//using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-//
-//namespace TestViewer
-//{		
-//	TEST_CLASS(ProcessorTest)
-//	{
-//	public:
-//		
-//		TEST_METHOD(TestComputeMetricsOK)
-//		{
-//			double expected = 0.0;
-//			double actual = Processor::computeMetrics(1.0, 1.0, 1.0, 1.0, 1.0, 1.0).returnedDouble;
-//			Assert::AreEqual(expected, actual);
-//		}
-//
-//	};
-//}
+#include "../ViewerExplorer/headers/Processor/Processor.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestViewer
+{		
+	TEST_CLASS(ProcessorTest)
+	{
+	public:
+		
+		TEST_METHOD(TestComputeMetricsOK)
+		{
+			double expected = 0.0;
+			double actual = Processor::computeMetrics(1.0, 1.0, 1.0, 1.0, 1.0, 1.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+	};
+}
