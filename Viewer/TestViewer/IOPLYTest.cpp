55cfa87c1fca07712d51ccf6b686277ebb3e090c
Antoine ROLLIN <antoine.rollin26@free.fr>
2017-01-31 16:09:51 +0100

    ADD Tests Viewer

---
+++ Viewer/TestViewer/IOPLYTest.cpp
@@ -0,0 +1,24 @@
+//#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "IOPLY.h"
+
+#include <iostream>
+#include <fstream>  
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestViewer
+{		
+	TEST_CLASS(IOTest)
+	{
+	public:
+		
+		TEST_METHOD(LoadOK1)
+		{
+			//pcl::PolygonMesh::Ptr mesh(new PolygonMesh());
+			//IOPLY::load("toTest.ply", mesh);		
+		}
+	};
+}
--- Viewer/TestViewer/ProcessorTest.cpp
+++ Viewer/TestViewer/ProcessorTest.cpp
@@ -1,7 +1,7 @@
 //#include "stdafx.h"
 #include "CppUnitTest.h"
 
-#include "../ViewerExplorer/headers/Processor/Processor.h"
+#include "Processor.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,12 +11,60 @@ namespace TestViewer
 	{
 	public:
 		
-		TEST_METHOD(TestComputeMetricsOK)
+		TEST_METHOD(TestComputeMetricsOK1)
 		{
 			double expected = 0.0;
 			double actual = Processor::computeMetrics(1.0, 1.0, 1.0, 1.0, 1.0, 1.0).returnedDouble;
 			Assert::AreEqual(expected, actual);
 		}
 
+		TEST_METHOD(TestComputeMetricsOK2)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(1.0, 0.0, 0.0, 0.0, 0.0, 0.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsOK3)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 1.0, 0.0, 0.0, 0.0, 0.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsOK4)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 0.0, 1.0, 0.0, 0.0, 0.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsOK5)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 0.0, 0.0, 1.0, 0.0, 0.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsOK6)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 0.0, 0.0, 0.0, 1.0, 0.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsOK7)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 0.0, 0.0, 0.0, 0.0, 1.0).returnedDouble;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(TestComputeMetricsKO)
+		{
+			double expected = 1.0;
+			double actual = Processor::computeMetrics(0.0, 0.0, 0.0, 0.0, 0.0, 2.0).returnedDouble;
+			Assert::AreNotEqual(expected, actual);
+		}
 	};
 }
